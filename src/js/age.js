1e8c252ee3b8afeed8043f7ca25c3567dcdd6f2c
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-03-11 14:45:05 -0800

    Update dob property value.

--- src/js/age.js
+++ src/js/age.js
@@ -1,6 +1,6 @@
 export default class Age {
   constructor(dob, advocacy, allyship, contributions, inquisitiveness, interconnectedness, joyCultivation, kindness, openHeartedness, personalGrowth, respect, selfCare, selfReflection, sharing, valuesInAction) {
-    this.dob = (new Date(dob)).valueOf();
+    this.dob = new Date(dob);
     this.advocacy = parseInt(advocacy);
     this.allyship = parseInt(allyship);
     this.contributions = parseInt(contributions);
