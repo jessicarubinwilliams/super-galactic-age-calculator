ffde8d1938e4ac6f41ea76ccb490c0327d7195e3
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-03-11 13:25:47 -0800

    Add reusableAge instance and good fail for Age constructor.

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -1,1 +1,16 @@
 import Age from './../src/js/age.js'
+
+describe('Age', () => {
+  let reusableAge;
+  
+  beforeEach(() => {
+    reusableAge = new Age("July 12, 1997", "10", "10", "10", "10", "10", "10", "10", "10", "10", "10", "10", "10", "10", "10");
+  });
+
+  test('should create Age instance', () => {
+    expect(reusableAge).toEqual({ dob: "Sat Jul 12 1997 00:00:00 GMT-0700 (Pacific Daylight Time)", advocacy: 10, allyship: 10, contributions: 10, inquisitiveness: 10, interconnectedness: 10, joyCultivation: 10, kindness: 10, openHeartedness: 10, personalGrowth: 10, respect: 10, selfCare: 10, selfReflection: 10, sharing: 10, valuesInAction: 10 })
+  });
+
+});
+
+
