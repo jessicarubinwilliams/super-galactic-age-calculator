2c04f0372fff70e78eac060818200013eea7a93f
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-03-11 16:29:12 -0800

    Add missing semicolon.

--- src/main.js
+++ src/main.js
@@ -2,4 +2,4 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Age from './js/age.js'
+import Age from './js/age.js';
