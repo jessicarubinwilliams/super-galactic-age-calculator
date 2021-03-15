ec288b28a2e26e79f8c39c8e6c4d12101a99848e
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-03-15 12:07:58 -0700

    Add gh-pages dependency & configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: "/super-galactic-age-calculator/"
   },
   devtool: 'eval-source-map',
   devServer: {               
